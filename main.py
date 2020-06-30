0d140b0be9366e7959f73bd55155a2cb43f524d1
mikedelong <mikedelong@outlook.com>
2020-06-30 13:28:40 -0400

    feature: first working version to get data using the API

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logging import basicConfig
 from logging import getLogger
 from time import time
 
+from tweepy import API
 from tweepy import Cursor
 from tweepy import OAuthHandler
 from json import load
@@ -17,8 +18,15 @@ if __name__ == '__main__':
         settings = load(fp=settings_fp, )
     logger.info('settings: {}'.format(settings))
     api_key = settings['api_key']
-    logger.info('API key: {}'.format(api_key))
+    logger.info('API key: {}'.format(api_key, ), )
     api_secret_key = settings['api_secret_key']
-    logger.info('secret key: {}'.format(api_secret_key))
+    logger.info('secret key: {}'.format(api_secret_key, ), )
+    hash_tag = settings['hash_tag']
+
+    authorization = OAuthHandler(consumer_key=api_key, consumer_secret=api_secret_key, )
+    interface = API(auth_handler=authorization, wait_on_rate_limit=True, )
+    values = Cursor(interface.search, q=hash_tag,).items(10)
+    for value in values:
+        logger.info(value)
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
