51a78aeb2659d0706fb12a80811b266ab504b45a
mikedelong <mikedelong@outlook.com>
2020-06-30 13:20:59 -0400

    feature: get settings from our settings file

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from logging import basicConfig
 from logging import getLogger
 from time import time
 
+from tweepy import Cursor
+from tweepy import OAuthHandler
+from json import load
+
 # https://blog.f-secure.com/how-to-get-tweets-from-a-twitter-account-using-python-and-tweepy/
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
     basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=INFO, )
     logger.info('started.', )
+    with open(file='./settings.json', mode='r', ) as settings_fp:
+        settings = load(fp=settings_fp, )
+    logger.info('settings: {}'.format(settings))
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
