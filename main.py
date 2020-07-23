f41e8285cd9f934e268dfbe0e8eb382c990e776b
mikedelong <mikedelong@outlook.com>
2020-07-23 14:34:10 -0400

    cleanup: remove a completed TODO

--- main.py
+++ main.py
@@ -99,7 +99,6 @@ if __name__ == '__main__':
         logger.info('most common mentions: {}'.format(percentage_mentions, ), )
 
     if 'user_compare' in functions:
-        # todo make this a setting
         logger.info('getting {} days of data for {}'.format(days, left))
         left_tags, left_mentions = get_connections(left, datetime.now() - timedelta(days=days), )
         logger.info('getting {} days of data for {}'.format(days, right))
