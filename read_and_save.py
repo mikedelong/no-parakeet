9fab8f3c92ec8e39c5f5f08c09be65dad7a00c03
Mike DeLong <mikedelong@outlook.com>
2022-05-10 11:03:28 -0400

    feature: build the initial graph

--- read_and_save.py
+++ read_and_save.py
@@ -2,13 +2,15 @@ from json import load
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
-from time import time
 
+from arrow import now
 from tweepy import API
+from tweepy import Cursor
 from tweepy import OAuthHandler
+from tweepy import TweepyException
 
 if __name__ == '__main__':
-    time_start = time()
+    time_start = now()
     logger = getLogger(__name__)
     basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=INFO, )
     logger.info('started.', )
@@ -27,4 +29,38 @@ if __name__ == '__main__':
                                  access_token_secret=access_token_secret)
     api = API(auth=authorization, wait_on_rate_limit=True)
 
-    logger.info('total time: {:5.2f}s'.format(time() - time_start))
+    me = api.get_user(screen_name=settings['screen_names'][settings['screen_name_index']])
+    logger.info('ID: %d %s screen name: %s', me.id, me.id_str, me.name)
+    data = dict()
+    # add the root user to the data
+    data[me.id] = {
+        'id_str': me.id_str,
+        'name': me.name,
+        'follower count': me.followers_count,
+        'screen name': me.screen_name,
+        'retrieved_date': now()
+    }
+    user_list = [me.id_str]
+    follower_list = []
+    for user in user_list:
+        # todo refactor this to use a comprehension (?)
+        followers = []
+        try:
+            for page in Cursor(api.get_follower_ids, user_id=user).pages():
+                followers.extend(page)
+                logger.info('follower count: %s', len(followers))
+        except TweepyException as tweepy_exception:
+            logger.warning(tweepy_exception)
+            continue
+        follower_list.append(followers)
+        for follower in followers:
+            this = api.get_user(user_id=follower)
+            data[follower] = {
+                'id_str': this.id_str,
+                'name': this.name,
+                'follower count': this.followers_count,
+                'screen name': this.screen_name,
+                'retrieved_date': now()
+            }
+
+    logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
